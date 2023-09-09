16186f498ce0aebc6d29490866251e38317ca27e
AbdallaMon <abdotlos60@gmail.com>
2023-09-09 18:51:51 +0200

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,30 @@
 import { Routes, Route, Link } from "react-router-dom";
-import "./App.css";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { HomePage } from "./components/Home.page";
-import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
-import { SuperHeroesPage } from "./components/SuperHeroes.page";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useEffect } from "react";
+import axios from "axios";
 const queryClient = new QueryClient();
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/super-heroes">Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/super-heroes" element={<SuperHeroesPage />}></Route>
-          <Route
-            path="/rq-super-heroes"
-            element={<RQSuperHeroesPage />}
-          ></Route>
-          <Route path="/" element={<HomePage />}></Route>
-        </Routes>
-      </div>
-    </QueryClientProvider>
-  );
+  const options = {
+    method: "GET",
+    url: "https://pizza-and-desserts.p.rapidapi.com/pizzas",
+    headers: {
+      "X-RapidAPI-Key": "c8cd5ccb22mshadae1f15c2a1814p197a4fjsne85622990140",
+      "X-RapidAPI-Host": "pizza-and-desserts.p.rapidapi.com",
+    },
+  };
+
+  useEffect(() => {
+    async function fetch() {
+      try {
+        const response = await axios.request(options);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetch();
+  }, []);
 }
 
 export default App;
