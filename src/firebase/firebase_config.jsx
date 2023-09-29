add05f3317f44df74e22ae0c5afad541583493e0
AbdallaMon <abdotlos60@gmail.com>
2023-09-29 11:18:42 +0200

    g

--- src/firebase/firebase_config.jsx
+++ src/firebase/firebase_config.jsx
@@ -1,4 +1,4 @@
-import { initializeApp, firebase } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -14,4 +14,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const fireBaseData = firebase.firestore();
