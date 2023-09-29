489618446553de78ec494c16f47404f56fe0cd7b
AbdallaMon <abdotlos60@gmail.com>
2023-09-29 13:40:22 +0200

    fixed the error

--- src/firebase/firebase_config.jsx
+++ src/firebase/firebase_config.jsx
@@ -1,7 +1,8 @@
-import { getAuth } from "firebase/auth";
-import { initializeApp } from "firebase/app";
-// import firebase from "firebase/compat/app";
-// import "firebase/compat/auth";
+// import { initializeApp } from "firebase/compat/app";
+// import { getAuth } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDlxWr8LumdR2gcA9NYBjWPRsrxHvFaQXo",
@@ -12,8 +13,8 @@ const firebaseConfig = {
   appId: "1:4208272209:web:2573e6c165a6d561c3e970",
   measurementId: "G-HGXF530SKY",
 };
-// firebase.initializeApp(firebaseConfig);
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-// export const auth = firebase.auth();
+// export const auth = getAuth(app);
+export const auth = firebase.auth();
